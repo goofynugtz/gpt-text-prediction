26964fd470c8c1611cf6ca3a65e7cfdac79235cd
goofynugtz <rahulranjan25.RR@gmail.com>
2023-03-24 00:38:49 +0530

    minor changes

--- gptTextPrediction/settings.py
+++ gptTextPrediction/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = config('DEBUG') == 'True'
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
 # Application definition
