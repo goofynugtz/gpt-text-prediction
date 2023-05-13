79502cbb4b492e869fedda22a939721f37107d03
goofynugtz <rahulranjan25.RR@gmail.com>
2023-05-13 15:14:58 +0530

    Minor changes

--- frontend/components/ModelType.tsx
+++ frontend/components/ModelType.tsx
@@ -10,10 +10,10 @@ const ModelType = ({ model, setModel }: any) => {
         className={`button ${model === '/v1/shakespeare/' ? 'active' : ''}`}
         onClick={() => setModel('/v1/shakespeare/')
         }>Shakespeare</div>
-      <div
+      {/* <div
         className={`button ${model === '/v2/' ? 'active' : ''}`}
         onClick={() => setModel('/v2/')
-        }>GPT 2</div>
+        }>GPT 2</div> */}
     </div>
   )
 }
